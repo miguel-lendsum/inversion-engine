639f74e2b5ad6b632bc84325f9fa6e08aac317f4
Joao Felipe Santoro <joao.santoro.pinto@concrete.com.br>
2020-05-08 17:21:05 -0300

    Issue #167: Add Support to Azure App Configuration
    Add suport for fetching values from Azure App Configuration.
    To add it’s capabilities, AZURE_APP_CONFIGURATION_CONNECTION_STRING must be configured in the environment, optionally AZURE_SERVICE_LABEL can be set to filter App Configuratiion based on a service label.

---
+++ inversion-api/src/main/java/io/inversion/cloud/utils/Vault.java
@@ -0,0 +1,13 @@
+package io.inversion.cloud.utils;
+
+public class Vault {
+    private String uri;
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+}
